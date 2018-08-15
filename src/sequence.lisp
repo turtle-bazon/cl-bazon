dc8be90107d4ab3d634bb919bb9d1b0db6fa4337
Azamat S. Kalimoulline <turtle@bazon.ru>
2018-08-15 13:30:17 +0300

    seq/split compilation problems resolved

--- src/sequence.lisp
+++ src/sequence.lisp
@@ -4,18 +4,21 @@
 
 (defmacro seq/split (seq delims
                      &optional &key
-                                 (key #'identity)
-                                 (test #'eq test-defined?)
+                                 (key nil key-defined?)
+                                 (test nil test-defined?)
                                  (test-not nil test-not-defined?))
   (when (and test-defined? test-not-defined?)
     (error ":TEST and :TEST-NOT were both supplied."))
-  (let ((test-part (cond
+  (let ((key-part (when key-defined?
+                    `(:key ,key)))
+        (test-part (cond
                      ((and test-defined? (not test-not-defined?)) `(:test ,test))
                      ((and (not test-defined?) test-not-defined?) `(:test-not ,test-not))
                      (t '()))))
     (if (list? delims)
         `(split-sequence:split-sequence-if
           (lambda (element)
-            (member element ,delims :key ,key ,@test-part))
-          ,seq)
-        `(split-sequence:split-sequence ,delims ,seq :key ,key ,@test-part))))
+            (member element ,delims ,@key-part ,@test-part))
+          seq)
+        `(split-sequence:split-sequence
+          ,delims ,seq ,@key-part ,@test-part))))
